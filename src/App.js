ec47353a456fa2fb64d91dd0f625b257bc35ff81
wakesurfer2 <116484986+wakesurfer2@users.noreply.github.com>
2023-12-03 20:52:10 +0100

    a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value} says Peter 3 times!!!</div>;
+  return <div>Hello {value} says Peter 4 times!!!</div>;
 }
 
 export default App;
