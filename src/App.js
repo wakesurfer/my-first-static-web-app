84790ddc188c9c7bb84f08f30d80ea1251f7bd9e
wakesurfer2 <116484986+wakesurfer2@users.noreply.github.com>
2023-12-03 21:36:15 +0100

    Lägga till autenticering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+//import React from 'react';
+import React, { useEffect } from 'react';
 
+/*
 function App() {
   const value = 'World';
   return <div>Hello {value} says Peter 5 times!!!</div>;
 }
+*/
+
+function App() {
+  useEffect(() => {
+    // Autentiseringskoden här
+  }, []);
+
+  return (
+    <div className="App">
+      {/* Din app-innehåll här */}
+    </div>
+  );
 
 export default App;
