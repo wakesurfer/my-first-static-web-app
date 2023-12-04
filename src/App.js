7f2232924c44ced4a4d68b534883784e7d0acee0
wakesurfer2 <peter@the-karlssons.com>
2023-12-04 15:30:43 +0100

    Cleanup committed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  useEffect(() => {
-    // Autentiseringskoden här
-  }, []);
-
   return (
     <div className="App">
       {/* Din app-innehåll här */}
